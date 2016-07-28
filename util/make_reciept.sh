0a7cb458f038f250c3d5ca2cc705ffd4f992f0a2
Ankit Ranjan <me@ankit.io>
2016-07-28 15:35:30 -0700

    Multiple filetypes for reciepts

--- util/make_reciept.sh
+++ util/make_reciept.sh
@@ -3,6 +3,6 @@ set -o pipefail
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
 
-reciept=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32).png
+reciept=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32).${1##*.}
 cp $1 $BASEDIR/reciepts/$reciept
 echo $reciept
