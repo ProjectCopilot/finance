409149926fb9d1524ff58ddfb61875872b9a3f01
Ankit Ranjan <me@ankit.io>
2016-07-30 00:07:56 -0700

    Again with the typo. Fixes #2

--- util/make_receipt.sh
+++ util/make_receipt.sh
@@ -3,6 +3,6 @@ set -o pipefail
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
 
-reciept=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32).${1##*.}
-cp $1 $BASEDIR/reciepts/$reciept
-echo $reciept
+receipt=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32).${1##*.}
+cp -v "$1" "$BASEDIR/receipts/$receipt"
+echo $receipt
